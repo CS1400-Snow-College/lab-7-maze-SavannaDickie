beb6b342a8ba1975937eaaaa3b90a7420233032b
Savanna Dickie <personal3@SavannaMac.local>
2024-10-28 20:01:20 -0600

    Preliminaries and story file

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Savanna Dickie
+//10/27/2024
+//Lab 7 - MAZE
+
+using System.Data;
+
+Console.WriteLine("Hello Player! Welcome to the MAZE!!\nUse the arrows on your keyboard to navigate the maze.\n");
+//string[]
